c15bf3fe44d96f66fe04f6f33dfb4d47805da3c6
Spatulox <M78stormtrooper@laposte.net>
2023-11-22 18:28:40 +0100

    Create checkXTimeInternet()
    check w time if internet is available before returning error or connected

--- Functions/checkInternetCo.js
+++ Functions/checkInternetCo.js
@@ -13,8 +13,41 @@ export async function checkInternetCo() {
         internetCo = 1;
     } else {
         log('No internet connection, waiting 1 minute.');
-        await new Promise((resolve) => setTimeout(resolve, 60000));
+        try{
+          await new Promise((resolve) => setTimeout(resolve, 60000));
+        }
+        catch{
+          log('ERROR when awaiting the promise to await 1 minutes')
+        }
     }
 
   }
+}
+
+
+export async function checkXTimesInternetCo(xTime) {
+  let count = 0;
+
+  while (count < xTime || count !== 'Connected') {
+    
+    log('CheckingInternet')
+    const online = await isOnline();
+
+    if (online) {
+        log('Internet connection is available.');
+        count = 'Connected';
+        return 'Connected'
+    } else {
+      count ++
+        log('No internet connection, waiting 1 minute.');
+        try{
+          await new Promise((resolve) => setTimeout(resolve, 60000));
+        }
+        catch{
+          log('ERROR when awaiting the promise to await 1 minutes')
+        }
+    }
+
+  }
+  return 'Error'
 }
