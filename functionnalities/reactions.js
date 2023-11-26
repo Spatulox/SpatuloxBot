f09fd6129f45d3c78a68aa6fa6284d6caa449d2d
Spatulox <M78stormtrooper@laposte.net>
2023-11-26 12:22:29 +0100

    new reaction.js

--- functionnalities/reactions.js
+++ functionnalities/reactions.js
@@ -3,7 +3,7 @@ import { log } from '../Functions/functions.js'
 export async function addReactions(message){
 
 	try{
-		const regexUrl = /(https:\/\/www.youtu[a-zA-Z-_:.\/0-9]*)/ // Regex to searc for a link
+		const regexUrl = /https?:\/\/(www\.)?youtu(be\.com\/watch\?v=|\.be\/)[\w-]{11}/ // Regex to search for a link
 		if (message.channelId == "1132996287520841869" && regexUrl.test(message.content)){ //Channel Musiques à sauvegarder (télécharger) 
 			message.react('💾')
 			// log("Good channel, reacted with : 💾")
