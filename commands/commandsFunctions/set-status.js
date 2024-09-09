0a751e9da93b173b78b32b02b6df135bbfe2f498
Spatulox <M78stormtrooper@laposte.net>
2024-09-09 18:38:53 +0200

    update set status
    add the error embed and the color system

--- commands/commandsFunctions/set-status.js
+++ commands/commandsFunctions/set-status.js
@@ -1,5 +1,5 @@
 import {log} from "../../functions/functions.js";
-import {createEmbed, readyToSendEmbed} from "../../functions/embeds.js";
+import {createEmbed, createErrorEmbed, returnToSendEmbed} from "../../functions/embeds.js";
 
 export async function setStatus(client, interaction){
     try{
@@ -8,22 +8,20 @@ export async function setStatus(client, interaction){
             name: interaction.options.getString('new-status')
         })
 
-        const embed = createEmbed()
+        const embed = createEmbed("botColor")
         embed.title = "Set Status OK"
         embed.description = `Status switched for ${interaction.options.getString('new-status')}`
-        embed.color = 0xba06ae
-        await interaction.editReply(readyToSendEmbed(embed));
+        await interaction.editReply(returnToSendEmbed(embed));
         log(`Status switched for ${interaction.options.getString('new-status')}`)
         return
     }
     catch(err){
         log(`ERROR : Impossible to set the activity of the bot : ${err}`)
         try{
-            const embed = createEmbed()
+            const embed = createEmbed("red")
             embed.title = "Set Status CRASH"
             embed.description = `Impossible to set the activity of the bot : ${err}`
-            embed.color = 0xba06ae
-            await interaction.editReply(readyToSendEmbed(embed))
+            await interaction.editReply(returnToSendEmbed(embed))
             return
         } catch{
             
