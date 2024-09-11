8ab951e52945f4b5a4c732fd7af734510ec4ec31
Spatulox <M78stormtrooper@laposte.net>
2024-09-11 02:04:00 +0200

    update reminder.js
    now use the date-hour type

--- commands/commandsFunctions/reminder.js
+++ commands/commandsFunctions/reminder.js
@@ -1,8 +1,9 @@
-import {createEmbed, createErrorEmbed, returnToSendEmbed, waitPrivateEmbedOrMessage} from "../../functions/embeds.js";
-import {log} from "../../functions/functions.js";
+import { createEmbed, createErrorEmbed, returnToSendEmbed, waitPrivateEmbedOrMessage } from "../../functions/embeds.js";
+import { log } from "../../functions/functions.js";
 
-import {ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle} from 'discord.js';
-import {loadForm} from "../../form/formBuilder.js";
+import { TextInputStyle } from 'discord.js';
+import { loadForm } from "../../form/formBuilder.js";
+import { readJsonFile, writeJsonFileRework } from "../../functions/files.js";
 
 // ------------------------------------------------------------- //
 
@@ -34,35 +35,69 @@ export async function reminderCommand(interaction){
 // ------------------------------------------------------------- //
 
 export async function addReminder(client, interaction){
-    const nom = interaction.fields.getTextInputValue('nom');
-    const description = interaction.fields.getTextInputValue('description');
-    const dateStr = interaction.fields.getTextInputValue('date');
+    try{
+        const nom = interaction.fields.getTextInputValue('nom');
+        const description = interaction.fields.getTextInputValue('description');
+        const dateStr = interaction.fields.getTextInputValue('date-hour');
 
-    // Validez et parsez la date
-    const dateParts = dateStr.split('/');
-    const date = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+        if(!dateStr.includes("/") || !dateStr.includes(" ") || !dateStr.includes(":")){
+            await interaction.reply(returnToSendEmbed(createErrorEmbed("Date invalide. Veuillez utiliser le format 'JJ/MM/AAAA hh:mm' .")))
+            return
+        }
 
-    if (isNaN(date.getTime())) {
-        await interaction.reply(returnToSendEmbed(createErrorEmbed('Date invalide. Veuillez utiliser le format JJ/MM/AAAA.'), true));
-        return;
+        const [datePart, timePart] = dateStr.split(' ');
+        const [day, month, year] = datePart.split('/');
+        const [hours, minutes] = timePart.split(':');
+
+        // Notez que les mois dans l'objet Date sont indexés à partir de 0
+        const date = new Date(year, month - 1, day, hours, minutes);
+
+        if (isNaN(date.getTime())) {
+            interaction.reply(returnToSendEmbed(createErrorEmbed("Date invalide. Veuillez utiliser le format 'JJ/MM/AAAA hh:mm' ."), true));
+            return
+        }
+
+        const embed = createEmbed()
+        embed.title = "Événement créé :"
+        embed.fields.push({
+            name: `Nom`,
+            value: nom
+        })
+        embed.fields.push({
+            name: `Description`,
+            value: description
+        })
+        embed.fields.push({
+            name: `Date`,
+            value: date.toLocaleDateString()
+        })
+
+        let data = await readJsonFile("./reminders/reminder.json")
+        if(data === ["Error"]){
+            data = {}
+        }
+
+        if (!data[formattedDate]) {
+            data[formattedDate] = [];
+        }
+        const formattedDate = `${day.padStart(2, '0')}/${month.padStart(2, '0')}/${year}`;
+        const formattedTime = `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}`;
+
+        data[formattedDate].push({
+            hour: formattedTime,
+            name: nom,
+            description: description
+        });
+
+        if(await writeJsonFileRework("./reminders", "reminder.json", data)){
+            interaction.reply(returnToSendEmbed(embed, true));
+        } else {
+            interaction.reply(returnToSendEmbed(createErrorEmbed(`Impossible to save the reminder, plz check the writeJsonFileRework() function...}`)))
+        }
+    } catch (e) {
+        log(`ERROR : Impossible to execute the addReminder function : ${e}`)
+        interaction.reply(returnToSendEmbed(createErrorEmbed(e)))
     }
-    const embed = createEmbed("yellow")
-    embed.title = "Événement créé :"
-    embed.fields.push({
-        name: `Nom`,
-        value: nom
-    })
-    embed.fields.push({
-        name: `Description`,
-        value: description
-    })
-    embed.fields.push({
-        name: `Date`,
-        value: date.toLocaleDateString()
-    })
-
-    // Traitez les données ici (par exemple, stockez-les dans une base de données)
-    await interaction.reply(returnToSendEmbed(embed, true));
 }
 
 // ------------------------------------------------------------- //
