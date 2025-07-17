aeadc22f4de1155e2f068bfd7432e44d32c1da61
Spatulox <M78stormtrooper@laposte.net>
2025-07-17 10:52:22 +0200

    move requestLatestVideo to ts
    Implements a feature that automatically checks for new videos on specified YouTube channels and posts them to designated Discord channels.
    
    It reads channel configurations from JSON files, parses the YouTube RSS feed, and posts new videos with relevant information (title, link, author, upload date) to the corresponding Discord channel.
    
    Also includes error handling and logging.

---
+++ src/events/automatics/requestLatestYtbVideo.ts
@@ -0,0 +1,57 @@
+import { readJsonFile, addVideoToJsonFile, listJsonFile } from '../../functions/files.js';
+import { postMessage } from '../../functions/messages.js';
+import { log } from '../../functions/functions.js';
+import Parser, { Item } from 'rss-parser';
+import { Client } from 'discord.js';
+
+interface ChannelData {
+  ytbChannel: string;
+  videosId: string[];
+  name: string;
+  guildChannelToPostVideo: string;
+}
+
+export async function recupLatestVideo(client: Client): Promise<void> {
+  const files = await listJsonFile('./ytbChannels/');
+  for (const fileName of files) {
+    const data = await readJsonFile<ChannelData>(`ytbChannels/${fileName}`);
+    await checkYoutubeFeed(client, data, fileName);
+  }
+}
+
+async function checkYoutubeFeed(client: Client, data: ChannelData, filename: string): Promise<void> {
+  let YOUTUBE_RSS_URL = 'https://www.youtube.com/feeds/videos.xml?channel_id=' + data.ytbChannel;
+  const parser = new Parser();
+  try {
+    const feed = await parser.parseURL(YOUTUBE_RSS_URL);
+
+    // Tableau des vidéos à ajouter à la JSON
+    const addVideoIdToFile: string[] = [];
+
+    for (const entry of feed.items) {
+      // entry.id est normalement de la forme "yt:video:VIDEO_ID"
+      const videoId = entry.id?.split(':')[2];
+      if (!videoId) continue;
+
+      if (!data.videosId.includes(videoId)) {
+        const date = new Date(entry.pubDate ?? '');
+        const timestamp = Math.floor(date.getTime() / 1000);
+
+        const sentence = `# 🎵 __** ${entry.title} **__ 🎵\n> - https://www.youtu.be/${videoId}\n> - Author : ${data.name}\n> - Uploaded on ${date.toLocaleDateString()}, <t:${timestamp}:R>`;
+
+        log(`INFO : Posting Video ${entry.title} - ${videoId}`);
+        await postMessage(client, sentence, data.guildChannelToPostVideo);
+        addVideoIdToFile.push(videoId);
+      }
+    }
+
+    if (addVideoIdToFile.length === 0) {
+      log(`INFO : Nothing to add for ${data.name}`);
+    } else {
+      log('INFO : Adding new videos to JSON file');
+      await addVideoToJsonFile('./ytbChannels', filename, addVideoIdToFile);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la vérification du flux :', error);
+  }
+}
