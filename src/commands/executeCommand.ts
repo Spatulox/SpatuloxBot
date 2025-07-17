822d32e2a01fbaef77d1f45d62745c42284b838d
Spatulox <M78stormtrooper@laposte.net>
2025-07-17 14:18:23 +0200

    Updates executeCommand.ts: Type command interaction
    Ensures command interactions are correctly typed as ChatInputCommandInteraction.

--- src/commands/executeCommand.ts
+++ src/commands/executeCommand.ts
@@ -1,28 +1,28 @@
-// Slashes command in alphabetical order...
-import { ytbChannelCommand } from './commandsFunctions/ytb-channel.js'
-import { setStatus } from './commandsFunctions/set-status.js'
+import { createErrorEmbed, sendInteractionEmbed } from '../functions/embeds.js';
+import { ChatInputCommandInteraction, Client, CommandInteraction } from 'discord.js';
+
+import { ytbChannelCommand } from './commandsFunctions/ytb-channel.js';
+import { setStatus } from "./commandsFunctions/set-status.js";
 import { reminderCommand } from "./commandsFunctions/reminder.js";
 
 import ytb from "./json/addytbchannel.json" with {type: "json"}
 import status from "./json/setstatus.json" with {type: "json"}
 import reminder from "./json/reminder.json" with {type: "json"}
-import { createErrorEmbed, sendInteractionEmbed } from '../functions/embeds.js';
-import { Client, CommandInteraction } from 'discord.js';
 
 export async function executeSlashCommand(interaction: CommandInteraction, client: Client){
     if (!interaction.isCommand()) return;
 
     switch (interaction.commandName) {
         case ytb.name:
-            ytbChannelCommand(client, interaction)
+            ytbChannelCommand(interaction as ChatInputCommandInteraction)
             break;
 
         case status.name:
-            setStatus(client, interaction)
+            setStatus(client, interaction as ChatInputCommandInteraction)
             break;
 
         case reminder.name:
-            reminderCommand(interaction)
+            reminderCommand(interaction as ChatInputCommandInteraction)
             break;
 
         default:
